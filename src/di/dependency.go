bd74e32a6683559696b35a4eb5868f54af156d9e
ryotaro oda <plm23.ccc@gmail.com>
2019-12-19 02:12:13 +0900

    feat: base

---
+++ src/di/dependency.go
@@ -0,0 +1,12 @@
+package di
+
+import "context"
+
+// Dependency ... dependency
+type Dependency struct {
+}
+
+// Inject ... indect dependency
+func (d *Dependency) Inject(ctx context.Context) {
+
+}
